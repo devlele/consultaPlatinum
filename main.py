3af846a7b49af9977aef275d216b7eeac49ce075
Alefe Freitas Santos <alefe.santos@afinz.com.br>
2025-03-03 14:26:20 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def read_df():
+    dataBase_df = pd.read_excel("Teste.xlsx")
+    CPF = dataBase_df['CPF']
+    print(CPF)
+    
+
+if __name__ == "__main__":
+    read_df()
