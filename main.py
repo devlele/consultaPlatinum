24e5137bc1610b7d9f61d2b6b48a5257696664ad
Alefe Freitas Santos <alefe.santos@afinz.com.br>
2025-03-05 15:54:32 -0300

    Função de leitura do arquivo parquet

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pandas as pd
 
 def read_df():
-    dataBase_df = pd.read_excel("Teste.xlsx")
+    try:
+        #dataBase_df = pd.read_excel("Teste.xlsx") #a função read_excel le o excel e transforma em um data freame
+        #dataBase_df.to_parquet("teste.parquet")
+        dataBase_df = pd.read_parquet("teste.parquet")#a função read_parquet le o arquivo parquet e transforma em um data freame
+        print(dataBase_df['CPF'])
+        print("Arquivo lido e convertido com sucesso!")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
 
-    CPF = dataBase_df['CPF']
-    print(CPF)
+    
+
+    
     
 
 if __name__ == "__main__":
